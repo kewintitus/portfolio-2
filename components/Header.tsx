a730baf4a00ed2c2c69ff67818b3046c0886b78e
Kewin Titus <kewintitus@gmail.com>
2022-10-26 16:35:37 +0530

    added animation of header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,13 +1,23 @@
 import React from 'react';
 
 import { SocialIcon } from 'react-social-icons';
+import { motion } from 'framer-motion';
 
 type Props = {};
 
 function Header({}: Props) {
   return (
-    <header className=" flex justify-between sticky top-0 max-w-7xl mx-auto">
-      <div className="flex flex-row items-center justify-left">
+    <header className=" flex justify-between sticky top-0 p-5 max-w-7xl mx-auto xl:items-center">
+      <motion.div
+        initial={{
+          x: -500,
+          opacity: 0,
+          scale: 0.5,
+        }}
+        animate={{ x: 0, opacity: 1, scale: 1 }}
+        transition={{ duration: 1.5 }}
+        className="flex flex-row items-center justify-left z-20"
+      >
         <SocialIcon
           url="https://twitter.com/"
           fgColor="gray"
@@ -20,8 +30,13 @@ function Header({}: Props) {
           bgColor="transparent"
           className=" cursor-pointer "
         />
-      </div>
-      <div className=" flex flex-row items-center content-center cursor-pointer">
+      </motion.div>
+      <motion.div
+        initial={{ x: 500, opacity: 0, scale: 0.5 }}
+        animate={{ x: 0, opacity: 1, scale: 1 }}
+        transition={{ duration: 1.5 }}
+        className=" flex flex-row items-center content-center cursor-pointer"
+      >
         <SocialIcon
           network="email"
           fgColor="gray"
@@ -31,7 +46,7 @@ function Header({}: Props) {
         <p className=" uppercase hidden md:inline-flex text-sm text-gray-400 ">
           Get in touch
         </p>
-      </div>
+      </motion.div>
     </header>
   );
 }
